bdef786df8f9b1f356cabf32e75a66c134bc3bf7
Dylan Zago <dylan.zago82@gmail.com>
2025-08-09 21:37:17 +0100

    Add a method (overtime dec. stats, water), refine evolution function and thresholds and add a mood function

--- crittercaretaker.py
+++ crittercaretaker.py
@@ -1,26 +1,83 @@
 # Critter Caretaker Module
 # This module defines the Critter class and its methods for managing critters.
+  
 
-def plantSeed(initial_state="seed"):
+def plantSeed(self):
+    evolution = self.happiness
+    if  evolution > 0:
         print("///////////")
         print("///--O--///")
+        print("///////////\n")
+    elif  evolution > 100:
+        print("     |     ")
+        print("///--O--///")
         print("///////////")
+        print("///////////\n")
+    elif evolution > 200:
+        print("     |     ")
+        print("     |     ")
+        print("///--O--///")
+        print("///////////\n")
+    elif evolution > 300:
+        print("   // \\    ")
+        print("  -- o --   ")
+        print("   \\|//    ")
+        print("     |      ")
+        print("///-/-\-///")
+        print("///////////\n")
+
+def mood(self):
+    unhappiness = self.water + self.happiness
+    if unhappiness < 5:
+        return "Your plant is thriving!"
+    elif 5 <= unhappiness < 10:
+        return "Your plant is doing okay, but could use some care."
+    elif 10 <= unhappiness < 15:
+        return "Your plant is unhappy and needs attention!"
+    
+
 
 class PlantCritter(object):
     """A virtual pet"""
     
-    def initialize(self):
+    def initialize(self, name, water=0, happiness=10):
         import time
         print("Planting...")
         time.sleep(2)
         print("Your plant is now ready to be cared for!")
         plantSeed()
-    
+        self.name = name # public attribute
+        self.water = water # public attribute
+        self.happiness = happiness # public attribute
 
+
+    def __stats__(self):
+        rep = "Plant critter object\n"
+        rep += "- Name:" + self.name + "\n"
+        return rep
+        
+    
+    def __pass_time(self):
+        self.water += 1
+        self.happiness -= 1
+
+    def water_plant(self, food= 4):
+        print("Glug glug... Your plant has been watered.")
+        self.water -= food
+        if self.water < 0:
+            self.water = 0
+        self.__pass_time()
+    
+    
+        
+     
 
 #Main
 pcrit = PlantCritter()
-pcrit.initialize()
+pcrit.initialize("Fernie")
+print(pcrit)
+print(pcrit.mood)
+
 input("\n\nPress enter to exit.")
 
 
